03e9ed1fa52a455e3dd903726954a9b24742c7c8
Milena <mi_lky@mail.ru>
2023-05-27 11:02:32 +0300

    fix path to ejs

--- src/app.js
+++ src/app.js
@@ -15,9 +15,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
