255e409da283a96dd69a21dd025711c9cbc51057
Milena <mi_lky@mail.ru>
2023-05-27 11:28:44 +0300

    fix path to static files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
-const fs = require('fs');
 const path = require('path');
 
 const db = require('./database/db');
@@ -13,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
